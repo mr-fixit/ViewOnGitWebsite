7689b80f850b57b431acc5041ad1c95169877634
Eric Slosser <eric.slosser@v-fx.com>
2020-07-14 07:24:26 -0400

    fix: path with space, pwd already at base, convert func in other file

--- VIG.sh
+++ VIG.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-source ./convert_char_offsets_to_line_numbers
-
-set $(osascript \
+AS_RESULT=$(osascript \
 -e 'tell application "Xcode"' \
 -e '   set n to name of front window' \
 -e '   set o to offset of " — " in n' \
 -e '   if o > 0 then set n to text 1 thru (o - 1) of n' \
 -e '   set doc to source document named n' \
 -e '   set r to selected character range of doc' \
--e '   set out1 to path of doc' \
--e '   set out2 to first item of r - 1' \
--e '   set out3 to 2nd item of r' \
+-e '   set sel0 to first item of r - 1' \
+-e '   set sel1 to 2nd item of r' \
+-e '   set docPath to path of doc' \
 -e 'end tell' \
--e 'return out1 & " " & out2 & " " & out3' )
+-e 'return sel0 as string & " " & sel1 & " " & docPath' )
 
-FILEPATH="$1"
-FIRST=$2
-LAST=$3
+FIRST=`echo $AS_RESULT | cut -f 1 -d ' '`
+LAST=`echo $AS_RESULT | cut -f 2 -d ' '`
+offsetToFilePath=$((${#FIRST}+${#LAST}+2))
+FILEPATH=${AS_RESULT:offsetToFilePath}
 
-read LINE0 LINE1 <<< $(convert_char_offsets_to_line_numbers.sh $FILEPATH $FIRST $LAST)
+read LINE0 LINE1 <<< $(./convert_char_offsets_to_line_numbers.sh "$FILEPATH" $FIRST $LAST)
 
 cd `dirname "$FILEPATH"`
 DIR=`pwd`
-cd $(git rev-parse --show-cdup)
+relativePathToBaseDir=$(git rev-parse --show-cdup)
+if [[ -s $relativePathToBaseDir ]]; then
+    cd $relativePathToBaseDir
+fi
 BASE=`pwd`
 SUBPATH="${FILEPATH:${#BASE}}"
 
@@ -31,6 +33,6 @@ REPO_URL=$(git remote show origin | grep 'Fetch URL:' | cut -c 14- | sed -e 's|g
 BRANCH=$(git branch | cut -c 3-)
 
 URL="https://$REPO_URL/blob/${BRANCH}${SUBPATH}#L${LINE0}-L${LINE1}"
+URL=$(echo $URL | sed -e 's| |%20|g')
 echo $URL
-open $URL
-
+#open $URL
