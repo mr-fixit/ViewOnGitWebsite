5975488221a83e03caed9189746106064237af4b
Eric Slosser <eric.slosser@v-fx.com>
2020-07-14 07:24:45 -0400

    fix off-by-1

--- convert_char_offsets_to_line_numbers.sh
+++ convert_char_offsets_to_line_numbers.sh
@@ -4,17 +4,16 @@ NCHARS=0; NLINES=0
 unset L1; unset L2
 # echo CHAR0=$CHAR0 CHAR1=$CHAR1
 cat "$FILEPATH" | while read L; do
-((NLINES += 1 ))
-thisLength=$(( ${#L} + 1 ))
-# echo L:$NLINES C:$NCHARS lenL=$thisLength
-if [[ -z $L0 ]] && (( CHAR0 <= NCHARS + thisLength )); then
-  L0=$NLINES
-fi
-if [[ -z $L1 ]] && (( CHAR1 <= NCHARS + thisLength )); then 
-  L1=$NLINES
-#  if (( CHAR1 == NCHARS + thisLength )); then (( L1-- )); fi
-  echo $L0 $L1  # this  is the output of the function
-  break;
-fi; 
-((NCHARS += thisLength ))
+  ((NLINES += 1 ))
+  thisLength=$(( ${#L} + 1 ))
+  # echo L:$NLINES C:$NCHARS lenL=$thisLength
+  if [[ -z $L0 ]] && (( CHAR0 < NCHARS + thisLength )); then
+    L0=$NLINES
+  fi
+  if [[ -z $L1 ]] && (( CHAR1 < NCHARS + thisLength )); then 
+    L1=$NLINES
+    echo $L0 $L1  # this  is the output of the function
+    break
+  fi; 
+  ((NCHARS += thisLength ))
 done
