10bde9a7eee590171a84dbb6d983836ed28c6bb4
Eric Slosser <eric.slosser@v-fx.com>
2020-07-13 21:00:02 -0400

    improve applescript part

--- VIG.sh
+++ VIG.sh
@@ -4,7 +4,10 @@ source ./convert_char_offsets_to_line_numbers
 
 set $(osascript \
 -e 'tell application "Xcode"' \
--e '   set doc to source document 0' \
+-e '   set n to name of front window' \
+-e '   set o to offset of " — " in n' \
+-e '   if o > 0 then set n to text 1 thru (o - 1) of n' \
+-e '   set doc to source document named n' \
 -e '   set r to selected character range of doc' \
 -e '   set out1 to path of doc' \
 -e '   set out2 to first item of r - 1' \
